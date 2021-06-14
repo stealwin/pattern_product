bdf7c349329052a416b7d11a6cbdd3b007959950
Slava <slava@Slavas-MacBook-Air.local>
2021-06-14 20:42:00 +0300

    Comments block

--- js/main.js
+++ js/main.js
@@ -1,17 +1,47 @@
 
- let tabs =document.querySelectorAll(".tabs_item");
+/* Animating Tabs */
+function aniTabs(){
+    let tabs =document.querySelectorAll(".tabs_item");
 
-for( let tab of tabs){
-    if (!tab.classList.contains("tabs_item_active")){
-        tab.addEventListener('click', e=>{          
-            for (let prevTab of tabs){
-                if (prevTab.classList.contains("tabs_item_active")){
-                    prevTab.classList.remove("tabs_item_active");
+    for( let tab of tabs){
+        if (!tab.classList.contains("tabs_item_active")){
+            tab.addEventListener('click', e=>{          
+                for (let prevTab of tabs){
+                    if (prevTab.classList.contains("tabs_item_active")){
+                        prevTab.classList.remove("tabs_item_active");
+                    }
                 }
-            }
-            tab.classList.add("tabs_item_active");          
-        });
-
+                tab.classList.add("tabs_item_active");          
+            });
+    
+        }
+      
     }
-  
 }
+aniTabs();
+/*  */
+let slideIndex=1;
+function slideIndexInit(){
+    
+    showSlides(slideIndex);
+}
+
+slideIndexInit();
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+ 
+function showSlides(n){
+    
+    let slides = document.getElementsByClassName("worker_card");
+    if(n>slides.length) slideIndex=1;
+    if (n<1) slideIndex = slides.length;
+    for (let slide of slides){
+        slide.style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";  
+    slides[slideIndex-1].style.transition = "opacity 0.3s ease 0.3s"; 
+
+}
+
